1faba5eb10f142f290f779db88381cddfaca4c44
mouzey <58259486+mouzey@users.noreply.github.com>
2020-04-16 22:15:46 +0800

    Update rand.h

--- server/common/rand.h
+++ server/common/rand.h
@@ -11,8 +11,4 @@ public:
 	static uint32_t Rand32(uint32_t max=UINT32_MAX, uint32_t min=0);
 	static uint64_t Rand64(uint64_t max=UINT64_MAX, uint64_t min=0);
 	static void Seed();
-	template<T>
-	static void Rand(std::vector<T>&in,std::vector<T>&out){}
-	template<T>
-	static void Rand(const& std::vector<T>&in,std::vector<T>&out){}
 };
